63d81c6a23bd48191df69530081a08c627e371f1
Simon-Kai Garvie <skgarvie@gmail.com>
2018-08-11 10:03:13 +0200

    Unity Setup
    Created Unity Project + Added RuntimeGizmo plugin

---
+++ Assets/RuntimeGizmo/Objects/Enums.cs
@@ -0,0 +1,7 @@
+﻿namespace RuntimeGizmos
+{
+	public enum TransformSpace {Global, Local}
+	public enum TransformType {Move, Rotate, Scale}
+	public enum TransformPivot {Pivot, Center}
+	public enum Axis {None, X, Y, Z, Any}
+}
