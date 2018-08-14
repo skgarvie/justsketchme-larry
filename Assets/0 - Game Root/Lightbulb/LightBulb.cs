dba775fb604a18421599a41d4566e4f2bcd19b14
Herman <hfbmartinus@gmail.com>
2018-08-14 11:40:58 +0200

    Cleaned up the project structure and got the lights working like little badasses

---
+++ Assets/0 - Game Root/Lightbulb/LightBulb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBulb : MonoBehaviour {
+
+	public Light lightSource;
+
+	public Slider intensitySlider;
+	public Slider rangeSlider;
+
+  public void Start()
+  {
+			intensitySlider.onValueChanged.AddListener(delegate {IntensityChangeCheck(); });
+      rangeSlider.onValueChanged.AddListener(delegate {RangeChangeCheck(); });
+  }
+
+  public void IntensityChangeCheck()
+  {
+      lightSource.intensity = intensitySlider.value*2;
+  }
+
+	public void RangeChangeCheck()
+  {
+      lightSource.range = rangeSlider.value*10;
+  }
+}
